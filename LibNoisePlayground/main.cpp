27e8432bf5c40553b7f43039313deef9968f6b36
Johnny Verbeek <iredcraft@gmail.com>
2016-06-10 23:22:54 +0200

    Added ImGui + Rendering code

--- LibNoisePlayground/main.cpp
+++ LibNoisePlayground/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "glfw/include/glfw3.h"
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_glfw.h"
 
 // Error Callback for OpenGL
 static void errorCallback(int error, const char* desc)
@@ -17,16 +19,25 @@ int main()
 	GLFWwindow* window = glfwCreateWindow(1280, 720, "LibNoise Playground", NULL, NULL);
 	glfwMakeContextCurrent(window);
 
+	ImGui_ImplGlfw_Init(window, true);
+
+	ImVec4 clear_color = ImColor(114, 144, 154);
+
 	// Main Rendering Loop
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
 
+		ImGui_ImplGlfw_NewFrame();
+
+
+
 		int dW, dH;
 		glfwGetFramebufferSize(window, &dW, &dH);
 		glViewport(0, 0, dW, dH);
-		glClearColor(0, 255, 0, 255);
+		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
 		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui::Render();
 		glfwSwapBuffers(window);
 	}
 
